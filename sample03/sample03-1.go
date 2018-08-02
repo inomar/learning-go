da01620b19fc8bd35c1f2695db78cd04353e514a
MakotoInomata <inomar1486@gmail.com>
2018-08-02 23:55:30 +0900

    feat collection

--- sample03/sample03-1.go
+++ sample03/sample03-1.go
@@ -43,8 +43,56 @@ func main() {
 	　　太郎です
 	`
 	fmt.Printf("%v", rw)
+
+	ary := [4]int{2, 4, 6, 8}
+	fmt.Println(ary[0], ary[3])
+
+	var sa [2]string
+	fmt.Println(sa)
+
+	// 要素の省略
+	a1 := [...]int{5, 100}
+	fmt.Println(a1[1])
+	a1[1] = 500
+	fmt.Printf("%v\n", a1)
+
+	var in interface{}
+	fmt.Println(in)
+	in = 3
+	in = "太郎"
+	in = '山'
+	in = 1.34
+	fmt.Println(in)
+
+	fmt.Println(plus(2, 4))
+	hello()
+
+	q, r := div(5, 2)
+	fmt.Println(q, r)
+	q1, _ := div(5, 2)
+	_, q2 := div(6, 2)
+	fmt.Println(q1, q2)
+
+	noname := func(x, y int) int { return y * x }
+	fmt.Println(noname(3, 2))
+	fmt.Printf("%T\n", func(x, y int) int { return x + y })
+	fmt.Printf("%v\n", func(x, y int) int { return x + y }(4, 5))
 }
 
 func one() int {
 	return 1
+}
+
+func plus(x, y int) int {
+	return x + y
+}
+
+func hello() {
+	fmt.Println("Hello")
+}
+
+func div(a, b int) (int, int) {
+	q := a / b
+	r := a % b
+	return q, r
 }
